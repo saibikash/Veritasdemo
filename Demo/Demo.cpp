30d747f83c1967e2a9098ef1a281d24b21cae9e4
saibikash <Bikashkumar.panda@veritas.com>
2017-01-16 17:41:26 +0530

    initial commit

---
+++ Demo/Demo.cpp
@@ -0,0 +1,16 @@
+// Demo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"Hello World";
+	getch();
+	return 0;
+}
+
